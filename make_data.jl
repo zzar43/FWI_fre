44b37592682199fdec08424995d134dec3425f37
zzar43 <lida7980@gmail.com>
2018-06-18 20:35:11 -0600

    23123

--- make_data.jl
+++ make_data.jl
@@ -5,7 +5,7 @@ include("model_parameter.jl");
 @everywhere include("FWI_fre.jl");
 using JLD2, PyPlot;
 
-@time wavefield_true, recorded_data_true = scalar_helmholtz_solver(vel_true, source_multi, acq_fre, "all");
+@time wavefield_true, recorded_data_true = scalar_helmholtz_solver_parallel(vel_true, source_multi, acq_fre, "all");
 
 @save "data_compute/marmousi_data.jld2" wavefield_true recorded_data_true
 
