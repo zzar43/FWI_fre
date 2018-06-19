413c7d4c8a20f4c17a1fc93732905078bfe264c9
zzar43 <lida7980@gmail.com>
2018-06-18 20:01:45 -0600

    12

--- make_data.jl
+++ make_data.jl
@@ -1,4 +1,4 @@
-addprocs(2)
+# addprocs(2)
 
 include("model_parameter.jl");
 @everywhere include("scalar_helmholtz_solver.jl");
@@ -7,6 +7,6 @@ using JLD2, PyPlot;
 
 @time wavefield_true, recorded_data_true = scalar_helmholtz_solver_parallel(vel_true, source_multi, acq_fre, "all");
 
-@save "data_compute/three_layers_data.jld2" wavefield_true recorded_data_true
+@save "data_compute/marmousi_data.jld2" wavefield_true recorded_data_true
 
-matshow(real(reshape(wavefield_true[:,1,2],Nx,Ny)'), cmap="seismic", clim=[-2,2]); colorbar(); savefig("wavefield.png")
+matshow(real(reshape(wavefield_true[:,1,1],Nx,Ny)'), cmap="seismic"); colorbar(); savefig("wavefield.png")
--- model_parameter.jl
+++ model_parameter.jl
@@ -108,11 +108,11 @@ end
 # vel_true[:,12:end] = 3;
 # vel_init = ones(Nx,Ny);
 # h = 0.01;
-@load "data/three_layers.jld2" vel_true vel_init Nx Ny h
-# @load "data/marmousi.jld2" vel_true vel_init Nx Ny h
+# @load "data/three_layers.jld2" vel_true vel_init Nx Ny h
+@load "data/marmousi.jld2" vel_true vel_init Nx Ny h
 
 # PML
-pml_len = 7;
+pml_len = 50;
 pml_alpha = 1;
 Nx_pml = Nx + 2pml_len;
 Ny_pml = Ny + 2pml_len;
@@ -124,23 +124,23 @@ dt = 1/sample_fre;
 Nt = 1000;
 t = linspace(0,(Nt-1)*dt,Nt);
 fre = sample_fre * linspace(0,1-1/Nt,Nt);
-fre_position = 3:2:20;
+fre_position = 11:11;
 frequency = fre[fre_position];
 fre_num = length(frequency);
 println("Frequency: ", frequency)
 
 # ===================================================
 # Source
-source_num = 12;
+source_num = 1;
 source_coor = zeros(Int,source_num,2);
 for i = 1:source_num
-    source_coor[i,1] = 1+ 20*(i-1);
+    source_coor[i,1] = 500;
     source_coor[i,2] = 1;
 end
-for i = 7:12
-    source_coor[i,1] = 1 + 20*(i-7);
-    source_coor[i,2] = 101;
-end
+# for i = 7:12
+#     source_coor[i,1] = 1 + 20*(i-7);
+#     source_coor[i,2] = 101;
+# end
 println("Source number: ", source_num)
 source_multi = build_source_multi(15,0.1,t,fre_position,source_num,fre_num,true);
 # ===================================================
