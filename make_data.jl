607b8bb291a4587b93178a0d8bac9824200587fd
zzar43 <lida7980@gmail.com>
2018-06-21 00:26:07 -0600

    make data

--- make_data.jl
+++ make_data.jl
@@ -1,13 +1,13 @@
 # addprocs(2)
 
-include("model_parameter.jl");
+# include("model_parameter.jl");
 @everywhere include("scalar_helmholtz_solver.jl");
-@everywhere include("FWI_fre.jl");
+# @everywhere include("FWI_fre.jl");
 using JLD2, PyPlot;
-@load "data/marmousi.jld2" vel_true vel_init Nx Ny h source_multi acq_fre
+@load "data/marmousi.jld2" vel_true vel_init acq_fre
 
-@time wavefield_true, recorded_data_true = scalar_helmholtz_solver(vel_true, source_multi, acq_fre, "all");
+@time wavefield_true, recorded_data_true = scalar_helmholtz_solver_parallel(vel_true, acq_fre, "all", true);
 
 @save "data_compute/marmousi.jld2" wavefield_true recorded_data_true
 
-matshow(real(reshape(wavefield_true[:,5,10],Nx,Ny)'), cmap="seismic"); colorbar(); savefig("temp_graph/wavefield.png")
+matshow(real(reshape(wavefield_true[:,5,10],acq_fre.Nx,acq_fre.Ny)'), cmap="seismic"); colorbar(); savefig("temp_graph/wavefield.png")
