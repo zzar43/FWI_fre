47b9f28d864722488eb981cf0859aa662d2b8c3b
zzar43 <lida7980@gmail.com>
2018-06-21 21:04:58 -0600

    777

--- model_parameter.jl
+++ model_parameter.jl
@@ -7,18 +7,24 @@ include("def_structure.jl");
 # Space
 # @load "data/three_layers.jld2" vel_true vel_init acq_fre
 # @load "data/marmousi.jld2" vel_true vel_init Nx Ny h
-# @load "data/overthrust.jld2" vel_true vel_init Nx Ny h
+# @load "data/overthrust.jld2" vel_true vel_init acq_fre
+@load "data/overthrust_small.jld2" vel_true vel_init
 
-using MAT;
-vars = matread("marmousi_dz10.mat");
-vel_true = vars["vel"]; vel_true = vel_true.';
-vel_true = convert(Array{Float32,2},vel_true)
-Nx, Ny = size(vel_true);
-h = 10;
-using ImageFiltering
-vel_init = imfilter(vel_true, Kernel.gaussian(15));
+# using MAT;
+# vars = matread("marmousi_dz10.mat");
+# vel_true = vars["vel"]; vel_true = vel_true.';
+# vel_true = convert(Array{Float32,2},vel_true)
+# Nx, Ny = size(vel_true);
+# h = 10;
+# using ImageFiltering
+# vel_init = imfilter(vel_true, Kernel.gaussian(15));
 
 # Nx = 101; Ny = 101; h = 0.01;
+# vel_true = vel_true[51:451,1:151];
+# vel_init = vel_init[51:451,1:151];
+
+h = 25;
+Nx = 401; Ny = 151;
 
 # PML
 pml_len = 50;
@@ -33,17 +39,17 @@ dt = 1/sample_fre;
 Nt = 1000;
 t = linspace(0,(Nt-1)*dt,Nt);
 fre = sample_fre * linspace(0,1-1/Nt,Nt);
-fre_position = 4:4:20;
+fre_position = 2:2:16;
 frequency = fre[fre_position];
 fre_num = length(frequency);
 println("Frequency: ", frequency)
 
 # ===================================================
 # Source
-source_num = 19;
+source_num = 81;
 source_coor = zeros(Int,source_num,2);
 for i = 1:source_num
-    source_coor[i,1] = 1+(i-1)*50;
+    source_coor[i,1] = 1+(i-1)*5;
     source_coor[i,2] = 1;
 end
 # for i = 7:source_num
@@ -52,7 +58,7 @@ end
 # end
 
 println("Source number: ", source_num)
-source_multi = build_source_multi(15,0.1,t,fre_position,source_num,fre_num,true);
+source_multi = build_source_multi(10,0.1,t,fre_position,source_num,fre_num,true);
 # ===================================================
 # Receiver
 receiver_num = Nx;
@@ -72,4 +78,4 @@ R_pml = build_proj_op_pml(Nx,Ny,receiver_coor,receiver_num,pml_len);
 # Make acquisition
 acq_fre = acquisition_fre(Nx,Ny,h,Nt,dt,t,frequency,fre_num,source_num,source_coor,source_multi,receiver_num,receiver_coor,R,R_pml,pml_len,pml_alpha,Nx_pml,Ny_pml);
 
-@save "data/marmousi.jld2" vel_true vel_init acq_fre
+# @save "data/overthrust_small.jld2" vel_true vel_init
